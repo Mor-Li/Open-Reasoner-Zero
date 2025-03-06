d02906ddee548aa5d15e539d9602a0fe39307d43
Mor-Li <2568818204@qq.com>
2025-03-06 12:44:16 +0000

    update run script

---
+++ docker/docker_run.sh
@@ -0,0 +1,31 @@
+# sudo docker run --gpus all -it \
+#     --name orz_container \
+#     --ipc=host \
+#     -v $(pwd):/workspace \
+#     -w /workspace \
+#     fs-computility-cn-beijing.cr.volces.com/devinstance-archive/orz:latest /bin/bash
+
+# sudo docker run --gpus all -it \
+#     --name orz_container \
+#     --ipc=host \
+#     --privileged \
+#     -v $(pwd):/workspace \
+#     -w /workspace \
+#     fs-computility-cn-beijing.cr.volces.com/devinstance-archive/orz:latest /bin/bash
+
+
+
+# Remove existing container if it exists
+if sudo docker ps -a --format '{{.Names}}' | grep -q '^orz_container$'; then
+    sudo docker rm -f orz_container
+fi
+sudo docker run --gpus all -it \
+    --name orz_container \
+    --ipc=host \
+    --tmpfs /dev/mqueue \
+    -v $(pwd):/workspace:rw \
+    -w /workspace \
+    fs-computility-cn-beijing.cr.volces.com/devinstance-archive/orz:latest /bin/bash
+
+export HF_ENDPOINT=https://hf-mirror.com
+DEBUG_MODE=True python -m playground.orz_14m_ppo_mini
