305e49dd69fff9ffe4f348e0b9110f02fe13eb6f
Mor-Li <2568818204@qq.com>
2025-03-06 19:15:25 +0800

    update Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,7 @@
+docker build --platform=linux/amd64 \
+    --build-arg HTTP_PROXY=http://10.10.43.220:7890 \
+    --build-arg HTTPS_PROXY=http://10.10.43.220:7890 \
+    -t open-reasoner-zero .
+
+docker build --platform=linux/amd64 \
+    -t open-reasoner-zero .
